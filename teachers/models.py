b6e1c1082570db334fab4fb400524b4b8986250e
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-03-17 23:48:04 +0530

    Updated Django files

---
+++ teachers/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class teachers(models.Model):
+    name = models.CharField(max_length=200)
+    dpt = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
