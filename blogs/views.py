9b891deaace49b533cf817a569c1a5c4f7241dfa
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-03-20 23:01:49 +0530

    Updated

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def addposts(request):
+    return HttpResponse("<h1>Add Posts</h1>")
+
+def addnews(request):
+    return HttpResponse("<h1>Add News<h1>")
