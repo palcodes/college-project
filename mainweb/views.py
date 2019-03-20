ff28b6ce2876562cf53f965a1136bf19ecc179cb
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-03-21 04:05:34 +0530

    Update

--- mainweb/views.py
+++ mainweb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    HttpResponse("<h1>Hey this is the home page</h1>")
+
+def about(request):
+    HttpResponse("<h1>Hey this is the about page</h1>")
+
+def comp(request):
+    HttpResponse("<h1>Hey this is the comps page</h1>")
--- workcollege/urls.py
+++ workcollege/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('mainweb.urls')),
     path('admin/', admin.site.urls),
     path('teachers/', include('teachers.urls')),  
     path('blogs/', include('blogs.urls'))
