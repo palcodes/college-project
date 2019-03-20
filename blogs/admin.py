f4aa2ca8f209fa9a4ec68741abb4d0f7cec1ba4a
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-03-21 04:02:26 +0530

    Updated

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import posts
+from .models import posts, news
 
 # Register your models here.
 admin.site.register(posts)
+admin.site.register(news)
--- blogs/models.py
+++ blogs/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class posts(models.Model):
-    title = models.CharField(max_length=500)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now=True)
+    posts_title = models.CharField(max_length=500)
+    posts_content = models.TextField()
+    posts_date = models.DateTimeField(auto_now=True)
 
 class news(models.Model):
-    title = models.CharField(max_length=500)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now=True)
+    news_title = models.CharField(max_length=500)
+    news_content = models.TextField()
+    news_date = models.DateTimeField(auto_now=True)
