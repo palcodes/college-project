18d8c245e27ff6b54e5b5801b88d15c491e4604f
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-03-23 23:10:21 +0530

    Update settings.py

--- workcollege/settings.py
+++ workcollege/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'teachers.apps.TeachersConfig',
     'blogs.apps.BlogsConfig',
+    'mainweb.apps.MainwebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
